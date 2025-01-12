ff911e9bb4a2fe6e4ed2c7ce19014cb8e1ce87c4
Tiebe Groosman <tiebe.groosman@gmail.com>
2025-01-12 16:29:41 +0100

    Add touch support

--- src/input-shim.cpp
+++ src/input-shim.cpp
@@ -19,6 +19,23 @@
 #define RMPP_MAX_DIGI_X 11180
 #define RMPP_MAX_DIGI_Y 15340
 
+#define RM2_MAX_TOUCH_X 20967
+#define RM2_MAX_TOUCH_Y 15725
+#define RM2_MAX_PRESSURE 4095
+#define RM2_MAX_TOOLTYPE 1
+#define RM2_MAX_SLOT     31
+#define RM2_MAX_TRACKID  65535 
+
+#define RM2_MAX_TOUCH_MAJOR 255
+#define RM2_MAX_TOUCH_MINOR 255
+#define RM2_MIN_ORIENTATION -127
+#define RM2_MAX_ORIENTATION  127
+
+#define RMPP_MAX_TOUCH_X 2064
+#define RMPP_MAX_TOUCH_Y 2832
+#define RMPP_MAX_PRESSURE 255
+
+
 static std::vector<int> fdsTouchScreen;
 static std::vector<int> fdsDigitizer;
 
@@ -56,6 +73,39 @@ int inputShimClose(int fd, int (*realClose)(int fd)) {
     return INTERNAL_SHIM_NOT_APPLICABLE;
 }
 
+static int fakeOrOverrideAbsInfo(
+    int fd,
+    unsigned long request,
+    char *ptr,
+    int (*realIoctl)(int, unsigned long, char*),
+    int minVal,
+    int maxVal,
+    int fuzzVal,
+    int flatVal,
+    int resolutionVal)
+{
+    int status = realIoctl(fd, request, ptr);
+    if (status == -1) {
+        struct input_absinfo fake = {0};
+        fake.value = 0;
+        fake.minimum = minVal;
+        fake.maximum = maxVal;
+        fake.fuzz    = fuzzVal;
+        fake.flat    = flatVal;
+        fake.resolution = resolutionVal;
+        std::memcpy(ptr, &fake, sizeof(fake));
+        return 0;
+    } else {
+        struct input_absinfo *absinfo = (struct input_absinfo *) ptr;
+        absinfo->minimum    = minVal;
+        absinfo->maximum    = maxVal;
+        absinfo->fuzz       = fuzzVal;
+        absinfo->flat       = flatVal;
+        absinfo->resolution = resolutionVal;
+        return 0;
+    }
+}
+
 #define IS_MATCHING_IOCTL(dir, type, nr) ((request & ~(_IOC_SIZEMASK << _IOC_SIZESHIFT)) == (_IOC(dir, type, nr, 0)))
 #define IS_MATCHING_IOCTL_S(dir, type, nr, size) (request == (_IOC(dir, type, nr, size)))
 #define SHIM_INVOKE_ORIGINAL if((status = realIoctl(fd, request, ptr)) == -1) return status
@@ -63,20 +113,97 @@ int inputShimIoctl(int fd, unsigned long request, char *ptr, int (*realIoctl)(in
     int ioctlInternalSize = _IOC_SIZE(request);
 
     auto position = std::find(fdsTouchScreen.begin(), fdsTouchScreen.end(), fd);
-    if(position != fdsTouchScreen.end()) {
-        CERR << "Touch IOCTL: " << request << " " << std::endl;
+    if (position != fdsTouchScreen.end()) {
+        CERR << "Touchscreen IOCTL: " << request << std::endl;
         int status;
-        if(IS_MATCHING_IOCTL(_IOC_READ, 'E', 0x20)) {
-            CERR << "CAPS? " << ioctlInternalSize << std::endl;
-            SHIM_INVOKE_ORIGINAL;
-            int *a = (int *) ptr;
-            CERR << status << " " << *a << std::endl;
-            // Fake the touchscreen capabilities
-            // TODO: Touch is broken
-            *a |= (1 << 3);
-            *a |= (1 << 2);
-            return status;
+
+        if (IS_MATCHING_IOCTL_S(_IOC_READ, 'E', 0x40 + ABS_MT_POSITION_X, sizeof(input_absinfo))) {
+            return fakeOrOverrideAbsInfo(fd, request, ptr, realIoctl,
+                                        0, RM2_MAX_TOUCH_X,
+                                        100, 0, 0);
         }
+        if (IS_MATCHING_IOCTL_S(_IOC_READ, 'E', 0x40 + ABS_MT_POSITION_Y, sizeof(input_absinfo))) {
+            return fakeOrOverrideAbsInfo(fd, request, ptr, realIoctl,
+                                        0, RM2_MAX_TOUCH_Y,
+                                        100, 0, 0);
+        }
+        if (IS_MATCHING_IOCTL_S(_IOC_READ, 'E', 0x40 + ABS_MT_PRESSURE, sizeof(input_absinfo))) {
+            return fakeOrOverrideAbsInfo(fd, request, ptr, realIoctl,
+                                        0, RM2_MAX_PRESSURE,
+                                        0, 0, 0);
+        }
+        if (IS_MATCHING_IOCTL_S(_IOC_READ, 'E', 0x40 + ABS_MT_TOUCH_MAJOR, sizeof(input_absinfo))) {
+            return fakeOrOverrideAbsInfo(fd, request, ptr, realIoctl,
+                                        0, RM2_MAX_TOUCH_MAJOR,
+                                        0, 0, 0);
+        }
+        if (IS_MATCHING_IOCTL_S(_IOC_READ, 'E', 0x40 + ABS_MT_TOUCH_MINOR, sizeof(input_absinfo))) {
+            return fakeOrOverrideAbsInfo(fd, request, ptr, realIoctl,
+                                        0, RM2_MAX_TOUCH_MINOR,
+                                        0, 0, 0);
+        }
+        if (IS_MATCHING_IOCTL_S(_IOC_READ, 'E', 0x40 + ABS_MT_ORIENTATION, sizeof(input_absinfo))) {
+            int status = realIoctl(fd, request, ptr);
+            if (status < 0) {
+                struct input_absinfo fake;
+                std::memset(&fake, 0, sizeof(fake));
+                fake.minimum = RM2_MIN_ORIENTATION;
+                fake.maximum = RM2_MAX_ORIENTATION;
+                std::memcpy(ptr, &fake, sizeof(fake));
+                return 0;
+            } else {
+                // override
+                struct input_absinfo *absinfo = (struct input_absinfo*) ptr;
+                absinfo->minimum = RM2_MIN_ORIENTATION;
+                absinfo->maximum = RM2_MAX_ORIENTATION;
+                return 0;
+            }
+        }
+        if (IS_MATCHING_IOCTL_S(_IOC_READ, 'E', 0x40 + ABS_MT_SLOT, sizeof(input_absinfo))) {
+            return fakeOrOverrideAbsInfo(fd, request, ptr, realIoctl,
+                                        0, RM2_MAX_SLOT,
+                                        0, 0, 0);
+        }
+        if (IS_MATCHING_IOCTL_S(_IOC_READ, 'E', 0x40 + ABS_MT_TOOL_TYPE, sizeof(input_absinfo))) {
+            return fakeOrOverrideAbsInfo(fd, request, ptr, realIoctl,
+                                        0, RM2_MAX_TOOLTYPE,
+                                        0, 0, 0);
+        }
+        if (IS_MATCHING_IOCTL_S(_IOC_READ, 'E', 0x40 + ABS_MT_TRACKING_ID, sizeof(input_absinfo))) {
+            return fakeOrOverrideAbsInfo(fd, request, ptr, realIoctl,
+                                        0, RM2_MAX_TRACKID,
+                                        0, 0, 0);
+        }
+
+        unsigned cmdDir  = _IOC_DIR(request);
+        unsigned cmdType = _IOC_TYPE(request);
+        unsigned cmdNr   = _IOC_NR(request);
+        unsigned cmdSize = _IOC_SIZE(request);
+
+        // pretend like we support everything the rm2 supports
+        if (cmdDir == _IOC_READ && cmdType == 'E' && cmdNr == (0x20 + EV_ABS)) {
+            int status = realIoctl(fd, request, ptr);
+            if (status < 0) return status;
+
+            unsigned long *bits = (unsigned long*) ptr;
+
+            auto setBit = [&](int code) {
+                bits[code / (8*sizeof(long))] |= (1UL << (code % (8*sizeof(long))));
+            };
+
+            setBit(ABS_MT_POSITION_X);
+            setBit(ABS_MT_POSITION_Y);
+            setBit(ABS_MT_PRESSURE);
+            setBit(ABS_MT_TOUCH_MAJOR);
+            setBit(ABS_MT_TOUCH_MINOR);
+            setBit(ABS_MT_ORIENTATION);
+            setBit(ABS_MT_SLOT);
+            setBit(ABS_MT_TOOL_TYPE);
+            setBit(ABS_MT_TRACKING_ID);
+
+            return 0;
+        }
+
     }
 
     position = std::find(fdsDigitizer.begin(), fdsDigitizer.end(), fd);
@@ -84,21 +211,16 @@ int inputShimIoctl(int fd, unsigned long request, char *ptr, int (*realIoctl)(in
         CERR << "Digitizer IOCTL: " << request << std::endl;
 
         int status;
+
         if (IS_MATCHING_IOCTL_S(_IOC_READ, 'E', 0x40 + ABS_X, sizeof(input_absinfo))) {
-            SHIM_INVOKE_ORIGINAL;
-            CERR << "Query max X. Size: " << ioctlInternalSize << std::endl;
-            struct input_absinfo *org = (struct input_absinfo *) ptr;
-            org->maximum = RM2_MAX_DIGI_X;
-            CERR << "Set: " << org->maximum << std::endl;
-            return 0;
+            return fakeOrOverrideAbsInfo(fd, request, ptr, realIoctl,
+                                        0, RM2_MAX_DIGI_X,
+                                        0, 0, 0);
         }
         if (IS_MATCHING_IOCTL_S(_IOC_READ, 'E', 0x40 + ABS_Y, sizeof(input_absinfo))) {
-            SHIM_INVOKE_ORIGINAL;
-            CERR << "Query max Y. Size: " << ioctlInternalSize << std::endl;
-            struct input_absinfo *org = (struct input_absinfo *) ptr;
-            org->maximum = RM2_MAX_DIGI_Y;
-            CERR << "Set: " << org->maximum << std::endl;
-            return 0;
+            return fakeOrOverrideAbsInfo(fd, request, ptr, realIoctl,
+                                        0, RM2_MAX_DIGI_Y,
+                                        0, 0, 0);
         }
     }
     return INTERNAL_SHIM_NOT_APPLICABLE;
@@ -135,6 +257,46 @@ ssize_t inputShimRead(int fd, void *bfr, unsigned long size, ssize_t (*realRead)
                         evt->value = 0;
                     }
                     break;
+            }
+            evt++;
+        }
+        return size;
+    }
+
+    position = std::find(fdsTouchScreen.begin(), fdsTouchScreen.end(), fd);
+    if(position != fdsTouchScreen.end()) {
+        struct input_event *evt = (struct input_event *) bfr;
+        if((size % sizeof(input_event)) != 0) {
+            CERR << "Unexpected read size: " << size << ". Expected multiple of " << sizeof(input_event) << ". Passing to normal read." << std::endl;
+            return INTERNAL_SHIM_NOT_APPLICABLE;
+        }
+        SHIM_INVOKE_ORIGINAL;
+        for(int i = 0; i<(size / sizeof(input_event)) - 1; i++) {
+            switch (evt->code) {
+                case ABS_MT_POSITION_X: {
+                    int oldVal = evt->value;
+                    int newVal = RM2_MAX_DIGI_X - (oldVal * RM2_MAX_DIGI_X) / RMPP_MAX_TOUCH_X;
+                    evt->value = newVal;
+                    break;
+                }
+                case ABS_MT_POSITION_Y: {
+                    int oldVal = evt->value;
+                    int newVal = RM2_MAX_DIGI_Y - (oldVal * RM2_MAX_DIGI_Y) / RMPP_MAX_TOUCH_Y;
+                    evt->value = newVal;
+                    break;
+                }
+                case ABS_MT_PRESSURE: {
+                    int oldVal = evt->value;
+                    int newVal = (oldVal * RM2_MAX_PRESSURE) / RMPP_MAX_PRESSURE;
+                    evt->value = newVal;
+                    break;
+                }
+                case ABS_MT_TOOL_TYPE: {
+                    if (evt->value > 1) {
+                        evt->value = 1;
+                    }
+                    break;
+                }
             }
             evt++;
         }
