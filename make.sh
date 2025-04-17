538a215d0756ab13483ae4a1e01f08e64ba5f3c6
asivery <asivery@protonmail.com>
2025-04-18 01:26:01 +0200

    Migrate to CMake

--- make.sh
+++
@@ -1,3 +0,0 @@
-SOURCES="src/qtfb-client/qtfb-client.cpp src/shim.cpp src/fb-shim.cpp src/input-shim.cpp"
-aarch64-remarkable-linux-g++ --sysroot $SDKTARGETSYSROOT -fPIC -shared -o shim.so $SOURCES
-aarch64-remarkable-linux-g++ -D_32BITFIXEDINFO --sysroot $SDKTARGETSYSROOT -fPIC -shared -o shim-32bit-structs.so $SOURCES
